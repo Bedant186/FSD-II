27ef70c0d4f92ae263a9c782745cbb4df09fddd4
Bedant186 <bedantsinghania186@gmail.com>
2026-04-17 22:39:57 +0530

    Added coverage artifacts

--- experiment-6/vite.config.js
+++ experiment-6/vite.config.js
@@ -8,6 +8,10 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/setupTests.js",
-    pool: "threads"
+    pool: "threads",
+    coverage: {
+    provider: "v8",
+    reporter: ["text", "html"]
+  }
   }
 });
